930573e7e699df78d459be5204cd9d0eb682c734
Phillip Malboeuf <phil@phils.computer>
2025-06-09 11:26:32 -0400

    copy paste

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,59 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { OnScroll } from '@/components/animations'
+import { LE, LPE } from '@/components/entry'
+import { Icon } from '@/components/icon'
+import { PageTransition } from '@/components/page_transition'
+import { ContentService } from '@/services/content'
+import Link from 'next/link'
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+import { Metadata, ResolvingMetadata } from 'next'
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+export async function generateMetadata(
+): Promise<Metadata> {
+  const homepage = await ContentService.homepage()
+
+  return {
+    title: homepage.fields.title,
+    description: homepage.fields.description
+  }
+}
+
+export default async function Home() {
+  const homepage = await ContentService.homepage()
+  return <>
+    <PageTransition />
+    <main role='main'>
+      <div className='padded padded--big_top relative nooverflow'>
+        <Icon i='anvil_homepage' />
+
+        <h2
+          data-parallax="2"
+          className='max_width max_width--tight'>
+          <OnScroll><LE c={homepage} k='description' /></OnScroll>
+        </h2>
+        <div className='big_bottom' />
+
+        <div className='grid grid--thick_guttered grid--spaced_around grid--middle'>
+          {homepage.fields.projects.map((project: any, index: number)=> <div key={project.fields.url}
+            data-parallax={2 - (index % 3 === 0 ? 0.5 : index % 3 === 1 ? 1 : 0)}
+            className={`col col--${homepage.fields.projectsGridSizes[index]}of12 col--tablet_portrait--12of12`}>
+            <Link href={`/projets/${project.fields.url}`}>
+              <OnScroll>
+                <div className='small_bottom'><LPE c={project} k='hero' /></div>
+                <h4>
+                  <LE c={project} k='title' />
+                </h4>
+              </OnScroll>
+            </Link>
+
+            <div className='normal_bottom hide_on_tablet_portrait' />
+          </div>)}
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        
+      </div>
+    </main>
+
+    <div className='padded padded--thick overflow_top text_center teal_back' style={{ position: 'relative', zIndex: 1 }}>
+      <OnScroll><Link href='/projets' className='big' style={{ zIndex: 1 }}><LE c={homepage} k='cta' /></Link></OnScroll>
     </div>
-  );
+  </>
 }
