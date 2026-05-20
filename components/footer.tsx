729fce34d90ea07ce0614d507f94a2637db3eeed
maudeturgeon <148978702+maudeturgeon@users.noreply.github.com>
2026-05-20 10:27:11 -0400

    test_footer_addlogo_1

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,38 +1,62 @@
 'use client'
-
 import Link from 'next/link'
 import * as React from 'react'
-
-
 import { Icon } from './icon'
 
-interface Props {
-}
-interface State {
-}
+interface Props {}
+interface State {}
 
 export class Footer extends React.Component<Props, State> {
-
   constructor(props: Props) {
     super(props)
-    this.state = {
-    }
+    this.state = {}
   }
 
-  componentDidMount() {
-  }
+  componentDidMount() {}
 
   render() {
     return <>
+      <style>{`
+        /* Chantier ES logo column */
+        .footer-chantier {
+          display: flex;
+          align-items: flex-end;
+          justify-content: flex-end;
+        }
+        .footer-chantier img {
+          width: 60px;
+          height: 60px;
+          object-fit: contain;
+        }
+
+        /* On mobile: hide from social column, show aligned right on email row */
+        .footer-chantier--desktop {
+          display: flex;
+        }
+        .footer-chantier--mobile {
+          display: none;
+        }
+
+        @media (max-width: 768px) {
+          .footer-chantier--desktop {
+            display: none;
+          }
+          .footer-chantier--mobile {
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+            margin-left: auto;
+          }
+        }
+      `}</style>
+
       <footer>
         <div className='grid grid--spaced grid--middle'>
-          
+
           <div className='grid grid--guttered grid--middle'>
-            {/* <div className='col hide_on_phone'><Link className='a--no_hover' href='/'><Icon i='logo' /></Link></div> */}
             <div className='col'><h4>coopérative en<br />aménagement<br />du territoire</h4></div>
             <div className='col col--tablet_portrait--12of12 phone_only' />
           </div>
-          
 
           <div className='grid grid--thick_guttered'>
             <div className='col col--tablet_portrait--12of12 hide_on_phone' />
@@ -41,8 +65,13 @@ export class Footer extends React.Component<Props, State> {
             </div>
             <div className='col'>
               <a href='mailto:info@enclume.ca' target='_blank'>info@enclume.ca</a>
+              {/* Mobile: logo appears right-aligned on same row as email */}
+              <span className='footer-chantier--mobile'>
+                <img src='/chantier-es.png' alt='Chantier de l'économie sociale' />
+              </span>
             </div>
           </div>
+
           <div className='grid grid--thick_guttered'>
             <div className='col'>
               <a href='https://goo.gl/maps/NpgUxc6ewHBePHk88' target='_blank'>4529 rue Clark,<br />
@@ -55,7 +84,12 @@ export class Footer extends React.Component<Props, State> {
               <a href='https://instagram.com/Enclume_atelier' target='_blank'>Instagram</a><br />
               <a href="https://www.linkedin.com/company/l'enclume---atelier-de-d-veloppement-territorial/" target='_blank'>LinkedIn</a>
             </div>
+            {/* Desktop/tablet: logo in its own column to the right of social links */}
+            <div className='col footer-chantier--desktop'>
+              <img src='/chantier-es.png' alt='Chantier de l'économie sociale' />
+            </div>
           </div>
+
         </div>
       </footer>
     </>
