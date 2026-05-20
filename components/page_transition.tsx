05cf442132afce128c87c985cf183b9177c8fe16
maudeturgeon <148978702+maudeturgeon@users.noreply.github.com>
2026-05-20 10:11:55 -0400

    Test_transition_noscroll

--- components/page_transition.tsx
+++ components/page_transition.tsx
@@ -1,10 +1,10 @@
 'use client'
-
 import { FunctionComponent, useEffect, useState } from 'react'
 import { Transition } from './animations'
 import { Icon } from './icon'
 
 interface Props {}
+
 export const PageTransition: FunctionComponent<Props> = props => {
   const [isBrowser, setIsBrowser] = useState(false)
   
@@ -15,7 +15,15 @@ export const PageTransition: FunctionComponent<Props> = props => {
   if (!isBrowser) return null
 
   const random = Math.floor(Math.random() * 3)
-  return <Transition keys={['anvil']} className={`grid grid--full grid--middle grid--center hero hero--transition hero--transition--${random} relative`}>
-    <Icon key={'anvil'} i={['anvil_transition_red', 'anvil_transition_green', 'anvil_transition_orange'][random]} />
-  </Transition>
+
+  return (
+    <div style={{ overflow: 'hidden', position: 'relative' }}>
+      <Transition
+        keys={['anvil']}
+        className={`grid grid--full grid--middle grid--center hero hero--transition hero--transition--${random} relative`}
+      >
+        <Icon key={'anvil'} i={['anvil_transition_red', 'anvil_transition_green', 'anvil_transition_orange'][random]} />
+      </Transition>
+    </div>
+  )
 }
