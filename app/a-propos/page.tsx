4bf37e534160e5212ac4ad0334407fb740b30816
maudeturgeon <148978702+maudeturgeon@users.noreply.github.com>
2026-07-06 09:16:19 -0400

    Updateapropos.tsx

--- app/a-propos/page.tsx
+++ app/a-propos/page.tsx
@@ -32,19 +32,6 @@ export default async function About() {
           </h2>
         </OnScroll>
 
-        {/* <OnScroll className='grid grid--guttered'>
-          <div className='col col--3of12 col--tablet_landscape--4of12 col--tablet_portrait--6of12 col--phone--12of12'
-            data-parallax="2"
-          >
-            <LRE c={about} k='introBodyLeft' />
-          </div>
-          <div className='col col--3of12 col--tablet_landscape--4of12 col--tablet_portrait--6of12 col--phone--12of12'
-            data-parallax="1"
-          >
-            <LRE c={about} k='introBodyRight' />
-          </div>
-        </OnScroll> */}
-
         <div className='big_bottom' />
 
         <OnScroll className='grid grid--thick_guttered'>
@@ -141,24 +128,6 @@ export default async function About() {
           <div className='col col--5of12 col--tablet_portrait--9of12 col--phone--12of12'>
             <LRE c={about} k='conclusion' />
           </div>
-        </OnScroll>
-
-        <OnScroll className='grid grid--tight_guttered'>
-          <div className='col col--12of12'
-            data-parallax="-1.5"
-          >
-            <h6><LE c={about} k='engagementsTitle' /></h6>
-          </div>
-          <div
-            data-parallax="-3"
-            className='col col--10of12 col--phone--12of12'><p className='big'><LE c={about} k='engagementsBody' /></p></div>
-
-          <div className='col col--12of12'></div>
-          {about.fields.engagements.map((engagement: { fields: any }, i: number) => <div key={i} className='col col--4of12 col--tablet_portrait--6of12 underline_links'
-            data-parallax="-0.5"
-          >
-            <LRE c={engagement} k='body' />
-          </div>)}
         </OnScroll>
 
         <div className='big_bottom' />
